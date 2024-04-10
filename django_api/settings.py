a2832d5fa491277b03cbc2331edb612ffa155c44
ExtintoMc <138510995+ExtintoMc@users.noreply.github.com>
2024-04-09 20:13:09 -0600

    problemas de cors

--- django_api/settings.py
+++ django_api/settings.py
@@ -145,7 +145,9 @@ CORS_ALLOWED_ORIGINS = [
     "https://sub.example.com",
     "http://192.168.1.103:5173",
     "http://127.0.0.1:5173",
-    "http://localhost:5173"
+    "http://127.0.0.1:8081",
+    "http://localhost:5173",
+    "http://localhost:8081"
 ]
 
 CORS_ORIGIN_WHITELIST = [
@@ -154,6 +156,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://192.168.1.103:5173",
     "http://127.0.0.1:5173",
     "http://localhost:5173"
+    "http://127.0.0.1:8081",
+    "http://localhost:8081"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
